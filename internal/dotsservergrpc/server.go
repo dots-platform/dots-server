ec80a73bc12ef169630a12fa15828b4164b3d146
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-04-06 08:43:03 -0700

    Establish server-to-server platform control communicator

--- internal/dotsservergrpc/server.go
+++ internal/dotsservergrpc/server.go
@@ -3,6 +3,8 @@ package dotsservergrpc
 import (
 	"context"
 
+	"github.com/google/uuid"
+
 	"github.com/dtrust-project/dtrust-server/internal/config"
 	"github.com/dtrust-project/dtrust-server/internal/serverconn"
 	"github.com/dtrust-project/dtrust-server/protos/dotspb"
@@ -12,8 +14,9 @@ import (
 const internalErrMsg = "Internal error"
 
 type DotsServerGrpc struct {
-	conns  serverconn.ServerConn
-	config *config.Config
+	conns       serverconn.ServerConn
+	controlComm *serverconn.ServerComm
+	config      *config.Config
 
 	dotspb.UnimplementedDecExecServer
 }
@@ -26,9 +29,19 @@ func NewDotsServerGrpc(nodeId string, config *config.Config) (*DotsServerGrpc, e
 		config: config,
 	}
 
+	// Establish server-to-server connection.
 	if err := server.conns.Establish(context.Background(), config); err != nil {
+		log.Error("Failed to establish server-to-server connection")
 		return nil, err
 	}
+
+	// Establish a communicator for control messages.
+	controlComm, err := server.conns.Register(context.Background(), serverconn.MsgTypePlatform, uuid.Nil)
+	if err != nil {
+		log.Error("Failed to establish server control communicator")
+		return nil, err
+	}
+	server.controlComm = controlComm
 
 	log.Info("Server connections established")
 
