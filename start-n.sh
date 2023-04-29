ed21ad0c9bd57057cfdd354623039b3a21c4ede1
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-04-29 09:41:42 -0700

    Add ability to specify further arguments to start-n.sh

--- start-n.sh
+++ start-n.sh
@@ -3,7 +3,7 @@
 set -eu
 
 usage() {
-    echo "Usage: $0 <num servers>"
+    echo "Usage: $0 <num servers> [-- [args...]]"
 }
 
 if [ $# -lt 1 ]; then
@@ -18,9 +18,16 @@ N=$1
 pids=
 trap 'kill $pids 2>/dev/null' INT QUIT TERM EXIT
 
+while [ "$#" -ge 1 -a "${1:?x}" != '--' ]; do
+    shift
+done
+if [ "$#" -ge 1 ]; then
+    shift
+fi
+
 i=0
 while [ "$i" -lt "$N" ]; do
-    go run ./cmd/dotsserver -config server_conf.yml -node_id "node$(( i + 1 ))" &
+    go run ./cmd/dotsserver -config server_conf.yml -node_id "node$(( i + 1 ))" "$@" &
     pids="$pids $!"
     i=$(( i + 1 ))
 done
